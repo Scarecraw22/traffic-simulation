e3d6986d6fdcb5849cb8ff62b78c41be90148d7d
Oskar Wasikowski <o.wasikowski@gmail.com>
2024-04-10 18:31:18 +0200

    Copy the same route file to each speed limit simulation

--- generateRandomTrips.sh
+++ generateRandomTrips.sh
@@ -1,7 +1,8 @@
 python .\\modifySpeedLimit.py
-python .\\randomTrips.py -n .\\20\\custom_20.net.xml -e 200 -r .\\20\\custom_20.routes.xml -l --begin 0 --end 500 --period 2
-python .\\randomTrips.py -n .\\30\\custom_30.net.xml -e 200 -r .\\30\\custom_30.routes.xml -l --begin 0 --end 800 --period 2
-python .\\randomTrips.py -n .\\40\\custom_40.net.xml -e 200 -r .\\40\\custom_40.routes.xml -l --begin 0 --end 800 --period 2
-python .\\randomTrips.py -n .\\50\\custom_50.net.xml -e 200 -r .\\50\\custom_50.routes.xml -l --begin 0 --end 800 --period 2
-python .\\randomTrips.py -n .\\60\\custom_60.net.xml -e 200 -r .\\60\\custom_60.routes.xml -l --begin 0 --end 800 --period 2
-python .\\randomTrips.py -n .\\70\\custom_70.net.xml -e 200 -r .\\70\\custom_70.routes.xml -l --begin 0 --end 800 --period 2
+python .\\randomTrips.py -n custom.net.xml -e 200 -r custom.routes.xml -l --begin 0 --end 800 --period 2
+cp custom.routes.xml .\\20\\custom_20.routes.xml
+cp custom.routes.xml .\\30\\custom_30.routes.xml
+cp custom.routes.xml .\\40\\custom_40.routes.xml
+cp custom.routes.xml .\\50\\custom_50.routes.xml
+cp custom.routes.xml .\\60\\custom_60.routes.xml
+cp custom.routes.xml .\\70\\custom_70.routes.xml
